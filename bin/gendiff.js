4767678933daeb3803b7d30b04bb1e4531af9653
Евгений Вульман <vulman@DESKTOP-0SKP5G9.localdomain>
2022-08-27 22:06:56 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,9 +5,11 @@ import { Command } from "commander";
 const program = new Command();
 
 program
-    .version('0.0.1', '-V, --version', 'output the version number')
+    .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
-    .help('-h, --help', 'display help for command')
+    .version('0.0.1')
+    .option('-f, --format <type>', 'output format', 'stylish')
+    .arguments('<filepath1> <filepath2>')
 
 program.parse();
 
