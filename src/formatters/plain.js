b8a9a0cc3a96f165229fb8d87c6a8fed5ab8bace
Евгений Вульман <vulman@DESKTOP-0SKP5G9.localdomain>
2022-09-14 21:11:00 +0300

    fix error and code-refactoring

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,37 +1,37 @@
-/* eslint-disable no-unreachable */
 import _ from 'lodash';
+
+const stringify = (value) => {
+  if ((_.isObject(value))) {
+    return '[complex value]';
+  }
+
+  if (typeof value === 'string') {
+    return `'${value}'`;
+  }
+
+  return value;
+};
 
 const plain = (tree) => {
   const iter = (node, acc) => {
-    const setValueText = (value) => {
-      if (_.isObject(value)) {
-        return '[complex value]';
-      }
-
-      if (typeof value === 'string') {
-        return `'${value}'`;
-      }
-
-      return value;
-    };
-
     const result = node.reduce((accum, obj) => {
       const name = acc ? `${acc}.${obj.name}` : obj.name;
       switch (obj.status) {
         case 'unchanged':
           return accum;
         case 'added':
-          return [...accum, `Property '${name}' was added with value: ${setValueText(obj.value)}`];
+          return [...accum, `Property '${name}' was added with value: ${stringify(obj.value)}`];
         case 'deleted':
           return [...accum, `Property '${name}' was removed`];
         case 'modified':
-          return [...accum, `Property '${name}' was updated. From ${setValueText(obj.valueBefore)} to ${setValueText(obj.valueAfter)}`];
-        default:
+          return [...accum, `Property '${name}' was updated. From ${stringify(obj.valueBefore)} to ${stringify(obj.valueAfter)}`];
+        case 'nested':
           return [...accum, iter(obj.children, `${name}`)];
+        default:
+          throw new Error(`Unknown state: '${obj.status}'!`);
       }
     }, []);
-
-    return result.join('\n');
+    return _.compact(result).join('\n');
   };
 
   return iter(tree, '');
